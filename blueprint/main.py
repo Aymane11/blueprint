89f7bb2768809c5bb47f3d4c38bf21d4a740d667
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-20 19:33:33 +0100

    :tada: Create first version
    This version contains the following features:
    - pre-commit hooks:
        - check-yaml
        - end-of-file-fixer
        - trailing-whitespace
        - check-added-large-files
        - detect-private-key
    - pytest with coverage
    - Linting using flake8 and pylint
    - Code style check using black
    - dotenv support

---
+++ blueprint/main.py
@@ -0,0 +1,8 @@
+secret_key = "secret"
+
+
+def say_hello():
+    return "Hello World!"
+
+
+assert list() == []  # nosec assert_used
---
+++ tests/test_blueprint.py
@@ -0,0 +1,5 @@
+from blueprint.main import say_hello
+
+
+def test_say_hello():
+    assert say_hello() == "Hello World!"
